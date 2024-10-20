be36107da79b28ce27865fd76f7f5c0fffecea71
nieves-mon <cunanan.nm@gmail.com>
2024-10-21 00:28:34 +0800

    add post api/shorturl endpoint

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let urlCount = 0;
+const urls = {};
+
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url;
+
+  if (urls[url]) {
+    res.json({ original_url: url, short_url: urls[url] });
+  }
+
+  if (!isValidUrl(url)) {
+    res.json({ error: "invalid url" });
+  }
+
+  urlCount++;
+  urls[url] = urlCount;
+
+  res.json({ original_url: url, short_url: urls[url] });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
